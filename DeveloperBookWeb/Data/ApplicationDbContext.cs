446c1c69d53fb95cdb4e9d4c1d603d89ded12df6
Malik El Guermai <malikelguermai@outlook.com>
2022-12-13 07:29:24 -0500

    first commit

---
+++ DeveloperBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DeveloperBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeveloperBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
